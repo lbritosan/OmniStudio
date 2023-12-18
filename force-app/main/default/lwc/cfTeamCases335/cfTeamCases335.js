2169ebc6914c3ba1e213d8f9fa98b8a9925edb2b
leonardobds_dev <leonardobds.dev@gmail.com>
2023-12-18 18:55:49 -0300

    Changes

---
+++ force-app/main/default/lwc/cfTeamCases335/cfTeamCases335.js
@@ -0,0 +1,56 @@
+import { FlexCardMixin } from "%vlocity_namespace%/flexCardMixin";
+    import {interpolateWithRegex, interpolateKeyValue, loadCssFromStaticResource } from "%vlocity_namespace%/flexCardUtility";
+    
+          import { LightningElement, api, track, wire } from "lwc";
+          import pubsub from "%vlocity_namespace%/pubsub";
+          import { getRecord } from "lightning/uiRecordApi";
+          
+          import data from "./definition";
+          
+          import styleDef from "./styleDefinition";
+              
+          export default class cfTeamCases335 extends FlexCardMixin(LightningElement){
+              @api debug;
+              @api recordId;
+              @api objectApiName;
+              
+              @track record;
+              
+              
+              pubsubEvent = [];
+              customEvent = [];
+              
+              connectedCallback() {
+                super.connectedCallback();
+                this.setStyleDefinition(styleDef);
+                data.Session = {} //reinitialize on reload
+                
+                
+                
+                this.setDefinition(data);
+ this.registerEvents();
+                
+                
+              }
+              
+              disconnectedCallback(){
+                super.disconnectedCallback();
+                    
+                    
+
+                  this.unregisterEvents();
+              }
+
+              registerEvents() {
+                
+              }
+
+              unregisterEvents(){
+                
+              }
+            
+              renderedCallback() {
+                super.renderedCallback();
+                
+              }
+          }
